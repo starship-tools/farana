9cd773481925bb8e7ad808b5a0e6a7ff61284669
Duncan McGreggor <oubiwann@starship.engineer>
2017-06-05 21:49:55 -0500

    Fixed metadata and added Felix dep.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject farana "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A Clojure Wrapper for Apache Felix (OSGi Framework)"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [
+    [org.apache.felix/org.apache.felix.framework "5.6.4"]
+    [org.clojure/clojure "1.8.0"]])
