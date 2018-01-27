fe2c22b3d9e87b2529b9c707cf19bce83d3a3fef
Duncan McGreggor <oubiwann@starship.engineer>
2018-01-27 16:21:15 -0600

    Updated URL.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject farana "0.1.0-SNAPSHOT"
   :description "A Clojure Wrapper for Apache Felix (OSGi Framework)"
-  :url "http://example.com/FIXME"
-  :license {:name "Apache License, Version 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :url "https://github.com/starship-tools/farana"
+  :license {
+    :name "Apache License, Version 2.0"
+    :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [
     [org.apache.felix/org.apache.felix.framework "5.6.4"]
     [org.clojure/clojure "1.8.0"]]
