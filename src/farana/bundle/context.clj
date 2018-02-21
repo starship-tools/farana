88b2834623662e7d9d5633cab00ae63aa45077d7
Duncan McGreggor <oubiwann@gmail.com>
2018-02-20 18:46:58 -0600

    Trying to figure out why I can't call some functions in OSGi bundles.

--- src/farana/bundle/context.clj
+++ src/farana/bundle/context.clj
@@ -4,7 +4,19 @@
   (:import
     (java.util Dictionary)
     (org.osgi.framework BundleContext
-                        ServiceListener)))
+                        ServiceListener))
+  (:gen-class))
+
+(defprotocol BundleContextAPI
+  (add-service-listener [this listener])
+  (remove-service-listener [this listener])
+  (register-service [this klass service properties])
+  (service-references [this service-name service-query])
+  (unget-service [this service-reference])
+  (property [this prop-key])
+  (service [this]))
+
+(defrecord FaranaBundleContext [])
 
 (defn add-service-listener
   [^BundleContext this ^ServiceListener listener]
@@ -21,6 +33,14 @@
                     service
                     properties))
 
+(defn service-references
+  [^BundleContext this ^String service-name ^String service-query]
+  (.getServiceReferences this service-name service-query))
+
+(defn unget-service
+  [^BundleContext this service-reference]
+  (.ungetService this service-reference))
+
 (defn property
   "The property key can be any type that the `name` function converts to a
   string."
@@ -30,3 +50,16 @@
 (defn service
   [^BundleContext this]
   (.getService this))
+
+(def behaviour
+  {:add-service-listener add-service-listener
+   :remove-service-listener remove-service-listener
+   :register-service register-service
+   :service-references service-references
+   :unget-service unget-service
+   :property property
+   :service service})
+
+(extend FaranaBundleContext
+        BundleContextAPI
+        behaviour)
